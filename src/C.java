c0a7bf752b877c4e9ae51c7b7cb4226eec724fc3
epochong <Valentineone@example.com>
2019-09-17 20:14:36 +0800

    ACM沈阳赛

---
+++ src/C.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * @author epochong
+ * @date 2019/9/8 10:08
+ * @email epochong@163.com
+ * @blog epochong.github.io
+ * @describe
+ */
+public class C {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/F.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * @author epochong
+ * @date 2019/9/8 9:34
+ * @email epochong@163.com
+ * @blog epochong.github.io
+ * @describe
+ */
+public class F {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);;
+        while (input.hasNextInt()) {
+
+        }
+    }
+}
